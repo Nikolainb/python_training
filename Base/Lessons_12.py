40115100e6b0a39f634d50cafce2946dcf5f5bed
Суворин Николай <sss-aaa-85@mail.ru>
2022-08-17 10:04:59 +0300

    Уроки и дз по python курсу,следущая часть

--- Base/Lessons_12.py
+++ Base/Lessons_12.py
@@ -1,4 +1,18 @@
-mytuple = tuple()
+mytuple = tuple() # функция tuple позволяет создать кортеж
 print(mytuple)
 mytuple = (1, '3', '5')
 print(mytuple)
+
+mytuple = tuple('Python')
+print(mytuple)
+
+
+#HW
+# Попросите пользователя ввести произвольную строку.
+#Создайте кортеж, состоящий из символов, введённой пользователем строки.
+#3) Выведите кортеж, используя цикл for.
+
+a = tuple(input("Введите произвольную строку:"))
+print(a)
+for b in a:
+    print(b)
